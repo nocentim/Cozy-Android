78b820906c76533de124b795cac93cbd358a6b6d
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-06-21 17:00:50 +0200

    onglets

--- gen/org/cozyAndroid/R.java
+++ gen/org/cozyAndroid/R.java
@@ -11,10 +11,38 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int calendar=0x7f020000;
+        public static final int deals=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int news=0x7f020003;
+        public static final int plus_orange=0x7f020004;
+        public static final int search=0x7f020005;
+        public static final int settings=0x7f020006;
+        public static final int tab_bg_selected=0x7f020007;
+        public static final int tab_bg_selector=0x7f020008;
+        public static final int tab_bg_unselected=0x7f020009;
+        public static final int tab_divider=0x7f02000a;
+        public static final int tab_text_selector=0x7f02000b;
+    }
+    public static final class id {
+        public static final int tabBPIcone=0x7f050005;
+        public static final int tabEventIcone=0x7f050003;
+        public static final int tabInfosIcone=0x7f050009;
+        public static final int tabNewsIcone=0x7f050001;
+        public static final int tabParamIcone=0x7f050007;
+        public static final int tab_bons_plans=0x7f050004;
+        public static final int tab_event=0x7f050002;
+        public static final int tab_infos=0x7f050008;
+        public static final int tab_news=0x7f050000;
+        public static final int tab_param=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int tab_calendrier=0x7f030001;
+        public static final int tab_liste=0x7f030002;
+        public static final int tab_plus=0x7f030003;
+        public static final int tab_recherche=0x7f030004;
+        public static final int tab_tags=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/org/cozyAndroid/CozyAndroidActivity.java
+++ src/org/cozyAndroid/CozyAndroidActivity.java
@@ -1,13 +1,47 @@
 package org.cozyAndroid;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class CozyAndroidActivity extends Activity {
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TabHost;
+
+public class CozyAndroidActivity extends TabActivity {
     /** Called when the activity is first created. */
+	private static TabHost tabHost;
+	private int [] layoutTab;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        tabHost = getTabHost();
+		layoutTab = new int[5];
+		layoutTab[0] = R.layout.tab_liste;
+		layoutTab[1] = R.layout.tab_calendrier;
+		layoutTab[2] = R.layout.tab_plus;
+		layoutTab[3] = R.layout.tab_tags;
+		layoutTab[4] = R.layout.tab_recherche;
+		
+		setupTab("TabListe", new Intent().setClass(this, TabListe.class),0);
+        setupTab("TabCalendrier", new Intent().setClass(this, TabCalendrier.class),1);
+        setupTab("TabPlus", new Intent().setClass(this, TabPlus.class),2);
+		setupTab("TabTags", new Intent().setClass(this, TabTags.class),3);
+		setupTab("TabRecherche", new Intent().setClass(this, TabRecherche.class),4);
     }
+    
+    private void setupTab(String tag, Intent intent, int layoutTabIndex) {
+		tabHost.addTab(tabHost.newTabSpec(tag).setIndicator( createTabView(tabHost.getContext(), layoutTabIndex)).setContent(intent));
+	}
+	 
+	// créé la vue associée à l'onglet considéré
+	private View createTabView(final Context context, int layoutTabIndex) {
+		View view = LayoutInflater.from(context).inflate(layoutTab[layoutTabIndex], null);
+		view.refreshDrawableState();
+		//view.setBackgroundResource(R.color.Ensimag);
+		return view;
+	}
 }
