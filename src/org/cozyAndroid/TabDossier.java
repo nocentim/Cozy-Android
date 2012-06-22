22498cfb848701cb491fbffa41989d19b9badf72
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-06-22 14:58:28 +0200

    ok

---
+++ src/org/cozyAndroid/TabDossier.java
@@ -0,0 +1,12 @@
+package org.cozyAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabDossier extends Activity {
+	
+	public void onCreate(Bundle saveInstanceState) {
+		super.onCreate(saveInstanceState);
+		setContentView(R.layout.tab_notes);
+	}
+}
