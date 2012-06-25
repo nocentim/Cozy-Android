6f055506c3eb50e7bf5e4186d9b8ffa31934aa37
grueld <tonton_38@hotmail.fr>
2012-06-25 10:39:23 +0200

    le bouton clear de l'onglet plus fonctionne :)

--- src/org/cozyAndroid/TabPlus.java
+++ src/org/cozyAndroid/TabPlus.java
@@ -1,12 +1,44 @@
 package org.cozyAndroid;
 
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class TabPlus extends Activity {
-	
+public class TabPlus extends Activity implements View.OnClickListener {
+	private EditText newText ;
+	Button clear   = null ;
+	Button valider = null ;
+
 	public void onCreate(Bundle saveInstanceState) {
-		super.onCreate(saveInstanceState);
-		setContentView(R.layout.tab_notes);
+		super.onCreate(saveInstanceState) ;
+		setContentView(R.layout.plus );
+
+		newText = (EditText)findViewById(R.id.bodyNewNote)  ;
+		clear   = (Button) findViewById(R.id.buttonClear)   ;
+		valider = (Button) findViewById(R.id.buttonValider) ;
+		
+		clear.setOnClickListener(this)   ;
+		valider.setOnClickListener(this) ;
 	}
+
+
+	@Override
+	public void onClick(View v) {
+	    switch(v.getId()) {
+
+	    case R.id.buttonClear : 
+	    	newText = (EditText)findViewById(R.id.bodyNewNote) ;
+			newText.setText("") ;
+	    	break ;
+	    
+	    case R.id.buttonValider :
+	    	//enregistrer la note dans la bd locale
+	    	break ;
+	    }
+	}
+
+
 } 
