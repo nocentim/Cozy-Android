430351bca4e958d0817aa78b4a0d67b8785418d0
Mathieu Nocenti <nocentim@ensimag.fr>
2012-06-26 15:53:23 +0200

    edition des notes dans une activité separée

--- src/org/cozyAndroid/TabListe.java
+++ src/org/cozyAndroid/TabListe.java
@@ -18,32 +18,19 @@ public class TabListe extends Activity {
 	
 	public void onCreate(Bundle saveInstanceState) {
 		super.onCreate(saveInstanceState);
-		}
-	
-	public void onResume() {
-		super.onResume();
 		setContentView(R.layout.liste_notes);
 		listeNotes = (ListView) findViewById(R.id.listNotes);
 		adapter = new NoteAdapter(this);
 		listeNotes.setAdapter(adapter);
+		
 		DataBase db = DataBase.getInstance();
 		ArrayList<Spanned> note = db.getAllNotes("notes","note");
 		adapter.setListe(note);
 		adapter.notifyDataSetChanged();
 	}
 	
-	public void modeEdition (String oldName, String oldBody) {
-		setContentView(R.layout.edition);
-		EditText name = (EditText) findViewById(R.id.nameEdition);
-		EditText body = (EditText) findViewById(R.id.bodyEdition);
-		name.setText(oldName);
-		body.setText(oldBody);
-		Button annuler = (Button) findViewById(R.id.buttonAnnuler);
-		annuler.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				//Retour à la liste
-				onResume();
-			}
-		});
+	public void onResume() {
+		super.onResume();
+		
 	}
 }
