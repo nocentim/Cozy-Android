d77e0a32ccae99cb8f5f7dc8508e65b3c90459f3
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-06-27 11:05:43 +0200

    affichage liste

--- src/org/cozyAndroid/TabListe.java
+++ src/org/cozyAndroid/TabListe.java
@@ -3,17 +3,13 @@ package org.cozyAndroid;
 import java.util.ArrayList;
 
 import org.cozyAndroid.Note.Notes;
-import org.cozyAndroid.providers.NotesProvider.DataBase;
 
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.ListView;
 
 public class TabListe extends Activity {
@@ -27,25 +23,20 @@ public class TabListe extends Activity {
 		listeNotes = (ListView) findViewById(R.id.listNotes);
 		adapter = new NoteAdapter(this);
 		listeNotes.setAdapter(adapter);
-
-		ArrayList<Spanned> note = new ArrayList<Spanned>();
-		Cursor cursor = managedQuery(Notes.CONTENT_URI,
-        		null, null, null, null);
-		if (cursor.moveToFirst()) {
-			do {
-				Spanned markedUp = Html.fromHtml(cursor.getString(0));
-				note.add(markedUp);
-			} while (cursor.moveToNext());
-		}
-		//DataBase db = DataBase.getInstance();
-		//ArrayList<Spanned> note = db.getAllNotes("notes","note");
-
-		adapter.setListe(note);
-		adapter.notifyDataSetChanged();
 	}
 	
 	public void onResume() {
 		super.onResume();
-		
-	}
+		ArrayList<Spanned> note = new ArrayList<Spanned>();
+		Cursor cursor = managedQuery(Notes.CONTENT_URI,null, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				Spanned markedUp = Html.fromHtml(cursor.getString(1));
+				note.add(markedUp);
+			} while (cursor.moveToNext());
+		}
+
+		adapter.setListe(note);
+		adapter.notifyDataSetChanged();
+		}
 }
