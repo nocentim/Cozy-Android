4fc459d11d0acd4ccc0a8cf5a4a15aa263a8da50
grueld <tonton_38@hotmail.fr>
2012-07-02 10:42:40 +0200

    on peut mettre en gras ou souligner ou mettre i en italique

--- gen/org/cozyAndroid/R.java
+++ gen/org/cozyAndroid/R.java
@@ -32,39 +32,39 @@ public final class R {
         public static final int tab_text_selector=0x7f020012;
     }
     public static final class id {
-        public static final int DeleteButton=0x7f060004;
-        public static final int bodyEdition=0x7f060007;
-        public static final int bodyNewNote=0x7f06000f;
-        public static final int body_note=0x7f06000c;
-        public static final int button1=0x7f060006;
-        public static final int buttonAnnuler=0x7f060008;
-        public static final int buttonBold=0x7f060010;
-        public static final int buttonClear=0x7f060012;
-        public static final int buttonEditer=0x7f060009;
-        public static final int buttonItalic=0x7f060013;
-        public static final int buttonUnderline=0x7f060014;
-        public static final int buttonValider=0x7f060011;
-        public static final int icone=0x7f06000a;
-        public static final int listNotes=0x7f06000d;
-        public static final int liste_navigateur=0x7f060003;
-        public static final int nameEdition=0x7f060005;
-        public static final int nameNewNote=0x7f06000e;
-        public static final int navigateur_path=0x7f060002;
-        public static final int precedent=0x7f060000;
-        public static final int suivant=0x7f060001;
-        public static final int tabBPIcone=0x7f06001f;
-        public static final int tabInfosIcone=0x7f060019;
-        public static final int tabListeIcone=0x7f06001c;
-        public static final int tabNewsIcone=0x7f060016;
-        public static final int tab_bons_plans=0x7f06001e;
-        public static final int tab_event=0x7f06001b;
-        public static final int tab_infos=0x7f060018;
-        public static final int tab_news=0x7f060015;
-        public static final int tbCalendrier=0x7f060017;
-        public static final int tbDossier=0x7f06001a;
-        public static final int tbListe=0x7f06001d;
-        public static final int tbPlus=0x7f060020;
-        public static final int titre_note=0x7f06000b;
+        public static final int DeleteButton=0x7f050004;
+        public static final int bodyEdition=0x7f050007;
+        public static final int bodyNewNote=0x7f05000f;
+        public static final int body_note=0x7f05000c;
+        public static final int button1=0x7f050006;
+        public static final int buttonAnnuler=0x7f050008;
+        public static final int buttonBold=0x7f050010;
+        public static final int buttonClear=0x7f050012;
+        public static final int buttonEditer=0x7f050009;
+        public static final int buttonItalic=0x7f050013;
+        public static final int buttonUnderline=0x7f050014;
+        public static final int buttonValider=0x7f050011;
+        public static final int icone=0x7f05000a;
+        public static final int listNotes=0x7f05000d;
+        public static final int liste_navigateur=0x7f050003;
+        public static final int nameEdition=0x7f050005;
+        public static final int nameNewNote=0x7f05000e;
+        public static final int navigateur_path=0x7f050002;
+        public static final int precedent=0x7f050000;
+        public static final int suivant=0x7f050001;
+        public static final int tabBPIcone=0x7f05001f;
+        public static final int tabInfosIcone=0x7f050019;
+        public static final int tabListeIcone=0x7f05001c;
+        public static final int tabNewsIcone=0x7f050016;
+        public static final int tab_bons_plans=0x7f05001e;
+        public static final int tab_event=0x7f05001b;
+        public static final int tab_infos=0x7f050018;
+        public static final int tab_news=0x7f050015;
+        public static final int tbCalendrier=0x7f050017;
+        public static final int tbDossier=0x7f05001a;
+        public static final int tbListe=0x7f05001d;
+        public static final int tbPlus=0x7f050020;
+        public static final int titre_note=0x7f05000b;
     }
     public static final class layout {
         public static final int dossier=0x7f030000;
@@ -79,22 +79,19 @@ public final class R {
         public static final int tab_plus=0x7f030009;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int bodyNewNote=0x7f050008;
-        public static final int hello=0x7f050000;
-        public static final int nameNewNote=0x7f050007;
-        public static final int path=0x7f05000b;
-        public static final int search_hint_dossier=0x7f05000d;
-        public static final int search_label_dossier=0x7f05000c;
-        public static final int sousTitreNote=0x7f05000a;
-        public static final int tabCalendrier=0x7f050003;
-        public static final int tabDossier=0x7f050005;
-        public static final int tabListe=0x7f050002;
-        public static final int tabPlus=0x7f050004;
-        public static final int tabRecherche=0x7f050006;
-        public static final int titreNote=0x7f050009;
-    }
-    public static final class xml {
-        public static final int searchable=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int bodyNewNote=0x7f040008;
+        public static final int hello=0x7f040000;
+        public static final int nameNewNote=0x7f040007;
+        public static final int path=0x7f04000b;
+        public static final int search_hint_dossier=0x7f04000d;
+        public static final int search_label_dossier=0x7f04000c;
+        public static final int sousTitreNote=0x7f04000a;
+        public static final int tabCalendrier=0x7f040003;
+        public static final int tabDossier=0x7f040005;
+        public static final int tabListe=0x7f040002;
+        public static final int tabPlus=0x7f040004;
+        public static final int tabRecherche=0x7f040006;
+        public static final int titreNote=0x7f040009;
     }
 }
--- src/org/cozyAndroid/TabPlus.java
+++ src/org/cozyAndroid/TabPlus.java
@@ -26,33 +26,29 @@ public class TabPlus extends Activity implements View.OnClickListener {
 	private Button italic = null;
 	private Button underline = null;
 
+	/**
+	 *  BIU pour: bold italic underligne, ecoute les boutons correspondants
+	 * @author bissou
+	 */
 	private View.OnClickListener BIUListener = new View.OnClickListener() {
-
 		public void onClick(View v) {
-			//TODO finir de mettre les autres sous classe en variable d'instance puis repercuter cette modification sur le code de onCreate()
-			// il faut aussi voir pour la gestion de la ligne commentée ci dessous et de ce que ca implique: la gestion du listener de l'edittext
-//			newText.removeTextChangedListener(TabPlus.TextListener.this) ;
+			newText.removeTextChangedListener(TextListener) ;
 			int selectionStart = newText.getSelectionStart();  // On récupère la sélection
 			int selectionEnd   = newText.getSelectionEnd();
 			if (selectionStart == -1) selectionStart = 0 ;
 			if (selectionEnd == -1) selectionEnd = 0 ;
-
-
 			Editable editable = newText.getText() ;
 			CharSequence baliseOuvrante = "", baliseFermante = "" ;
 
 			switch (v.getId()) {
-
 			case R.id.buttonBold :
 				baliseOuvrante = "<b>" ;
 				baliseFermante = "</b>" ;
 				break ;
-
 			case R.id.buttonItalic :
 				baliseOuvrante = "<i>" ;
 				baliseFermante = "</i>" ;
 				break ;
-
 			case R.id.buttonUnderline :
 				baliseOuvrante = "<u>" ;
 				baliseFermante = "</u>" ;
@@ -60,14 +56,48 @@ public class TabPlus extends Activity implements View.OnClickListener {
 			}
 
 			editable.insert(selectionStart, baliseOuvrante) ;    // On met la balise avant la sélection
+			newText.addTextChangedListener(TextListener) ;	
 			editable.insert(selectionEnd + 3 , baliseFermante) ; // On rajoute la balise après la sélection (et les 3 caractères de la balise <b>)
 			//TODO il faut revoir cette double insertion car cela modifie deux fois le texte donc appel deux fois le listener
-//			newText.addTextChangedListener(this) ;	
+			Log.d("allo", "onclicklistener") ;
+			Log.d("allo", newText.getText().toString()) ;
 		}
 	} ;
 
-	
-	
+	/**
+	 * Ecoute le bouton enter pour permettre a l'utilisateur de revenir
+	 * a la ligne dans ca note
+	 */
+	private View.OnKeyListener EnterListener = new View.OnKeyListener() {
+
+		public boolean onKey(View v, int keyCode, KeyEvent event) {
+			// On récupère la position du début de la sélection dans le texte
+			int cursorIndex = newText.getSelectionStart();
+			// Ne réagir qu'à l'appui sur une touche (et pas le relâchement)
+			if(event.getAction() == 0)
+				// S'il s'agit d'un appui sur la touche « entrée »
+				if(keyCode == 66)
+					// On insère une balise de retour à la ligne
+					newText.getText().insert(cursorIndex, "<br />");
+			return true;
+		}
+	} ;
+
+	/**
+	 * Ecoute les changements du texte
+	 */
+	private TextWatcher TextListener = new TextWatcher() {
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+		public void onTextChanged(CharSequence s, int start, int before, int count) {}
+		public void afterTextChanged(Editable s) {	
+			newText.removeTextChangedListener(this) ;
+			// Le Textview interprète le texte dans l'éditeur en une certaine couleur
+			newText.setText(Html.fromHtml(newText.getText().toString()));
+			//			newText.setText(Html.fromHtml("<font color=\"" + currentColor + "\">" + newText.getText().toString() + "</font>", null , null));
+			newText.addTextChangedListener(this) ;	
+		}
+	} ;
+
 	public void onCreate(Bundle saveInstanceState) {
 		super.onCreate(saveInstanceState) ;
 		setContentView(R.layout.plus );
@@ -82,33 +112,26 @@ public class TabPlus extends Activity implements View.OnClickListener {
 
 		// Pour mettre en gras
 		bold = (Button) findViewById(R.id.buttonBold) ;
-		bold.setOnClickListener(new BIUListener()) ;
+		bold.setOnClickListener(BIUListener) ;
 		// Pour mettre en italic
 		italic = (Button) findViewById(R.id.buttonItalic);
-		italic.setOnClickListener(new BIUListener());
+		italic.setOnClickListener(BIUListener);
 		// Pour souligner
 		underline = (Button) findViewById(R.id.buttonUnderline);
-		underline.setOnClickListener(new BIUListener());
+		underline.setOnClickListener(BIUListener);
 
-//		newText.setMovementMethod (new ScrollingMovementMethod()) ;
+		//		newText.setMovementMethod (new ScrollingMovementMethod()) ;
 		// On ajouter un Listener sur l'appui de touches
-		newText.setOnKeyListener(new EnterListener());
+		newText.setOnKeyListener(EnterListener);
 		// On ajoute un autre Listener sur le changement dans le texte cette fois
-		newText.addTextChangedListener(new TextListener());
-	}
-
-	public void onResume() {
-		super.onResume();
-		newText.setSelection(newText.getText().length(), newText.getText().length());
+		newText.addTextChangedListener(TextListener);
 	}
 
 	public void onClick(View v) {
 		switch(v.getId()) {
-
 		case R.id.buttonClear : 
 			newText.setText("") ;
 			break ;
-
 		case R.id.buttonValider :
 			///dataBase.addNote("Notes", "note", newName.getText() + ", body: " + newText.getText()) ;
 			ContentValues values = new ContentValues();
@@ -119,48 +142,5 @@ public class TabPlus extends Activity implements View.OnClickListener {
 			newName.setText("") ;
 			break ;
 		}
-
-
 	} 
-
-	/**
-	 *  BIU pour: bold italic underligne
-	 * @author bissou
-	 * 
-	 */
-	//TODO transformer cette classe en une variable d'instance
-	private class EnterListener implements View.OnKeyListener {
-
-		public boolean onKey(View v, int keyCode, KeyEvent event) {
-			// On récupère la position du début de la sélection dans le texte
-			Log.d("hello","je bug la");
-			int cursorIndex = newText.getSelectionStart();
-			// Ne réagir qu'à l'appui sur une touche (et pas le relâchement)
-			if(event.getAction() == 0)
-				// S'il s'agit d'un appui sur la touche « entrée »
-				if(keyCode == 66)
-					// On insère une balise de retour à la ligne
-					newText.getText().insert(cursorIndex, "<br />");
-			return true;
-		}
-	}
-
-	//TODO transformer cette classe en une variable d'instance
-	private class TextListener implements TextWatcher {
-
-
-		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-		public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-		public void afterTextChanged(Editable s) {	
-			newText.removeTextChangedListener(this) ;
-			// Le Textview interprète le texte dans l'éditeur en une certaine couleur
-			Log.d("allo", newText.getText().toString()) ;
-			newText.setText(Html.fromHtml(newText.getText().toString()));
-
-//			newText.setText(Html.fromHtml("<font color=\"" + currentColor + "\">" + newText.getText().toString() + "</font>", null , null));
-			newText.addTextChangedListener(this) ;	
-		}
-
-	}
 }
