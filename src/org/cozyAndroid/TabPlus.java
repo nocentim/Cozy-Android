732373448422bff0a1dfe68932c561a87670b973
grueld <tonton_38@hotmail.fr>
2012-07-02 09:21:43 +0200

    mise en gras

--- src/org/cozyAndroid/TabPlus.java
+++ src/org/cozyAndroid/TabPlus.java
@@ -6,10 +6,7 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.Html;
-import android.text.Spanned;
-import android.text.TextWatcher;
+import android.text.* ;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -29,6 +26,48 @@ public class TabPlus extends Activity implements View.OnClickListener {
 	private Button italic = null;
 	private Button underline = null;
 
+	private View.OnClickListener BIUListener = new View.OnClickListener() {
+
+		public void onClick(View v) {
+			//TODO finir de mettre les autres sous classe en variable d'instance puis repercuter cette modification sur le code de onCreate()
+			// il faut aussi voir pour la gestion de la ligne commentée ci dessous et de ce que ca implique: la gestion du listener de l'edittext
+//			newText.removeTextChangedListener(TabPlus.TextListener.this) ;
+			int selectionStart = newText.getSelectionStart();  // On récupère la sélection
+			int selectionEnd   = newText.getSelectionEnd();
+			if (selectionStart == -1) selectionStart = 0 ;
+			if (selectionEnd == -1) selectionEnd = 0 ;
+
+
+			Editable editable = newText.getText() ;
+			CharSequence baliseOuvrante = "", baliseFermante = "" ;
+
+			switch (v.getId()) {
+
+			case R.id.buttonBold :
+				baliseOuvrante = "<b>" ;
+				baliseFermante = "</b>" ;
+				break ;
+
+			case R.id.buttonItalic :
+				baliseOuvrante = "<i>" ;
+				baliseFermante = "</i>" ;
+				break ;
+
+			case R.id.buttonUnderline :
+				baliseOuvrante = "<u>" ;
+				baliseFermante = "</u>" ;
+				break ;
+			}
+
+			editable.insert(selectionStart, baliseOuvrante) ;    // On met la balise avant la sélection
+			editable.insert(selectionEnd + 3 , baliseFermante) ; // On rajoute la balise après la sélection (et les 3 caractères de la balise <b>)
+			//TODO il faut revoir cette double insertion car cela modifie deux fois le texte donc appel deux fois le listener
+//			newText.addTextChangedListener(this) ;	
+		}
+	} ;
+
+	
+	
 	public void onCreate(Bundle saveInstanceState) {
 		super.onCreate(saveInstanceState) ;
 		setContentView(R.layout.plus );
@@ -51,7 +90,7 @@ public class TabPlus extends Activity implements View.OnClickListener {
 		underline = (Button) findViewById(R.id.buttonUnderline);
 		underline.setOnClickListener(new BIUListener());
 
-		newText.setMovementMethod (new ScrollingMovementMethod()) ;
+//		newText.setMovementMethod (new ScrollingMovementMethod()) ;
 		// On ajouter un Listener sur l'appui de touches
 		newText.setOnKeyListener(new EnterListener());
 		// On ajoute un autre Listener sur le changement dans le texte cette fois
@@ -89,42 +128,7 @@ public class TabPlus extends Activity implements View.OnClickListener {
 	 * @author bissou
 	 * 
 	 */
-	private class BIUListener implements View.OnClickListener {
-
-		public void onClick(View v) {
-			int selectionStart = newText.getSelectionStart();  // On récupère la sélection
-			int selectionEnd   = newText.getSelectionEnd();
-			if (selectionStart == -1) selectionStart = 0 ;
-			if (selectionEnd == -1) selectionEnd = 0 ;
-
-
-			Editable editable = newText.getText() ;
-			CharSequence baliseOuvrante = "", baliseFermante = "" ;
-
-			switch (v.getId()) {
-
-			case R.id.buttonBold :
-				baliseOuvrante = "<b>" ;
-				baliseFermante = "</b>" ;
-				break ;
-
-			case R.id.buttonItalic :
-				baliseOuvrante = "<i>" ;
-				baliseFermante = "</i>" ;
-				break ;
-
-			case R.id.buttonUnderline :
-				baliseOuvrante = "<u>" ;
-				baliseFermante = "</u>" ;
-				break ;
-			}
-
-			editable.insert(selectionStart, baliseOuvrante) ;    // On met la balise avant la sélection
-			editable.insert(selectionEnd + 3 , baliseFermante) ; // On rajoute la balise après la sélection (et les 3 caractères de la balise <b>)
-			//TODO il faut revoir cette double insertion car cela modifie deux fois le texte donc appel deux fois le listener
-		}
-	}
-
+	//TODO transformer cette classe en une variable d'instance
 	private class EnterListener implements View.OnKeyListener {
 
 		public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -139,26 +143,22 @@ public class TabPlus extends Activity implements View.OnClickListener {
 					newText.getText().insert(cursorIndex, "<br />");
 			return true;
 		}
-
 	}
 
+	//TODO transformer cette classe en une variable d'instance
 	private class TextListener implements TextWatcher {
 
 		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-		
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			newText.removeTextChangedListener(this) ;
-			newText.setSelection(newText.getText().length(), newText.getText().length());
-			//			// Le Textview interprète le texte dans l'éditeur en une certaine couleur
-			Editable edit = newText.getText() ;
-			String sr = edit.toString() ;
-			Spanned sp = Html.fromHtml(sr) ;
-			//			newText.setText(Html.fromHtml("<font color=\"" + currentColor + "\">" + newText.getText().toString() + "</font>", null , null));
-			newText.addTextChangedListener(this) ;
-			
-		}
+		public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
 		public void afterTextChanged(Editable s) {	
+			newText.removeTextChangedListener(this) ;
+			// Le Textview interprète le texte dans l'éditeur en une certaine couleur
+			Log.d("allo", newText.getText().toString()) ;
+			newText.setText(Html.fromHtml(newText.getText().toString()));
+
+//			newText.setText(Html.fromHtml("<font color=\"" + currentColor + "\">" + newText.getText().toString() + "</font>", null , null));
+			newText.addTextChangedListener(this) ;	
 		}
 
 	}
