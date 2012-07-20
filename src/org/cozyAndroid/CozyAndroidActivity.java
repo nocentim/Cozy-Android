250b3bf32739b3ccb53cf86b1a8342bd62f80168
grueld <tonton_38@hotmail.fr>
2012-07-20 11:35:01 +0200

    deuxieme revert

--- src/org/cozyAndroid/CozyAndroidActivity.java
+++ src/org/cozyAndroid/CozyAndroidActivity.java
@@ -96,6 +96,7 @@ public class CozyAndroidActivity extends TabActivity  implements OnItemClickList
 
 	public void onResume(){
 		super.onResume();
+		
 	}
 
 
@@ -124,10 +125,6 @@ public class CozyAndroidActivity extends TabActivity  implements OnItemClickList
             }
         }, null, "1.0");
 	}*/
-
-	public static TabHost gettabHost(){
-		return tabHost;
-	}
 
 	protected void startEktorp() {
 		Log.v(TAG, "starting ektorp");
