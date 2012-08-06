2d9188748149e13f2f2149d67fa76ee6482fd012
nocentim <nocentim@ensimag.fr>
2012-08-06 12:58:47 +0200

    tri des suggestions selon le nombre d'occurences

--- src/org/cozyAndroid/SuggestionAdapter.java
+++ src/org/cozyAndroid/SuggestionAdapter.java
@@ -1,14 +1,21 @@
 package org.cozyAndroid;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.util.Comparators;
 import org.ektorp.CouchDbConnector;
+import org.ektorp.DbAccessException;
 import org.ektorp.ViewQuery;
+import org.ektorp.ViewResult;
 import org.ektorp.ViewResult.Row;
+import org.ektorp.android.util.ChangesFeedAsyncTask;
 import org.ektorp.android.util.CouchbaseViewListAdapter;
+import org.ektorp.android.util.EktorpAsyncTask;
+import org.ektorp.changes.ChangesCommand;
+import org.ektorp.changes.DocumentChange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import android.content.Context;
 import android.text.Spannable;
@@ -100,15 +107,6 @@ public class SuggestionAdapter extends CouchbaseViewListAdapter  implements Filt
 			String end = start.toUpperCase() + "\u9999";
 			SuggestionAdapter.this.viewQuery.startKey(start).endKey(end).group(true);
 			SuggestionAdapter.this.updateListItems();
-			Collections.sort(listRows, new Comparator<Row>() {
-
-				@Override
-				public int compare(Row lhs, Row rhs) {
-					Log.d("tri par valeur", "left : " + lhs.getValueAsInt() + " right : " + rhs.getValueAsInt());
-					return ((Integer)rhs.getValueAsInt()).compareTo(lhs.getValueAsInt());
-				}
-			});
-			listRows.add(new Row(listRows.get(0).getDocAsNode()));
 			res.count = getCount();
 			Log.d("filtrage suggestions","count = " + res.count);
 			res.values = null;
@@ -124,5 +122,112 @@ public class SuggestionAdapter extends CouchbaseViewListAdapter  implements Filt
             }
 		}
 		
+	}
+	
+	//Copié-collé du code de CouchbaseViewAdapter avec modification de updateListItem()
+	
+	private static final Logger LOG = LoggerFactory
+            .getLogger(CouchbaseViewListAdapter.class);
+	protected CouchbaseListChangesAsyncTask couchChangesAsyncTask;
+
+	protected void updateListItems() {
+		//if we're not already in the process of updating the list, start a task to do so
+		if(updateListItemsTask == null) {
+
+			updateListItemsTask = new EktorpAsyncTask() {
+
+				protected ViewResult viewResult;
+
+				@Override
+				protected void doInBackground() {
+					viewResult = couchDbConnector.queryView(viewQuery);
+				}
+
+				protected void onSuccess() {
+					if(viewResult != null) {
+						lastUpdateView = viewResult.getUpdateSeq();
+						listRows = viewResult.getRows();
+						Collections.sort(listRows, new Comparator<Row>() {
+
+							@Override
+							public int compare(Row lhs, Row rhs) {
+								Log.d("tri par valeur", "left : " + lhs.getValueAsInt() + " right : " + rhs.getValueAsInt());
+								return ((Integer)rhs.getValueAsInt()).compareTo(lhs.getValueAsInt());
+							}
+						});
+						notifyDataSetChanged();
+					}
+					updateListItemsTask = null;
+
+					//we want to start our changes feed AFTER
+					//getting our first copy of the view
+					if(couchChangesAsyncTask == null && followChanges) {
+						//create an ansyc task to get updates
+						ChangesCommand changesCmd = new ChangesCommand.Builder().since(lastUpdateView)
+								.includeDocs(false)
+								.continuous(true)
+								.heartbeat(5000)
+								.build();
+
+						couchChangesAsyncTask = new CouchbaseListChangesAsyncTask(couchDbConnector, changesCmd);
+						couchChangesAsyncTask.execute();
+					}
+
+					if(lastUpdateChangesFeed > lastUpdateView) {
+						if (LOG.isDebugEnabled()) {
+				            LOG.debug("Finished, but still behind " + lastUpdateChangesFeed + " > " + lastUpdateView);
+						}
+						updateListItems();
+					}
+
+				}
+
+				@Override
+				protected void onDbAccessException(
+						DbAccessException dbAccessException) {
+					handleViewAsyncTaskDbAccessException(dbAccessException);
+				}
+
+			};
+
+			updateListItemsTask.execute();
+		}
+	}
+
+	protected void handleViewAsyncTaskDbAccessException(DbAccessException dbAccessException) {
+		LOG.error("DbAccessException accessing view for list", dbAccessException);
+	}
+
+	private class CouchbaseListChangesAsyncTask extends ChangesFeedAsyncTask {
+
+		public CouchbaseListChangesAsyncTask(CouchDbConnector couchDbConnector,
+				ChangesCommand changesCommand) {
+			super(couchDbConnector, changesCommand);
+		}
+
+		@Override
+		protected void handleDocumentChange(DocumentChange change) {
+			lastUpdateChangesFeed = change.getSequence();
+			updateListItems();
+		}
+
+		@Override
+		protected void onDbAccessException(DbAccessException dbAccessException) {
+			handleChangesAsyncTaskDbAccessException(dbAccessException);
+		}
+
+	}
+
+	protected void handleChangesAsyncTaskDbAccessException(DbAccessException dbAccessException) {
+		LOG.error("DbAccessException following changes feed for list", dbAccessException);
+	}
+
+	/**
+	 * Cancel the following of continuous changes, necessary to properly clean up resources
+	 */
+	public void cancelContinuous() {
+		if(couchChangesAsyncTask != null) {
+			couchChangesAsyncTask.cancel(true);
+		}
 	}
 }
